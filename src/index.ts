12c0ae03ca9a9a7f21a3d917564a18961d656260
Emmanuel Obagunwa <access2emma@gmail.com>
2023-09-11 01:41:49 +0100

    chore: add support for group chat. added README.md

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import http from "http";
 import express from "express";
 import { Server } from "socket.io";
 import { UserInfo, UserPayload } from "./types/user";
+import { nanoid } from "nanoid";
 
 const app = express();
 const server = http.createServer(app);
@@ -13,7 +14,7 @@ app.use(cors({ credentials: true }));
 const io = new Server(server);
 
 let userList: UserInfo[] = [];
-let groups = [];
+let groups: string[] = [];
 
 app.get("/", (req, res) => {
 	res.send("Welcome to Codeo server");
@@ -31,6 +32,7 @@ io.on("connection", (socket) => {
 	});
 
 	socket.on("setup", (user: UserPayload) => {
+		if (!(user.name && user.phone)) return;
 		const existingUserIndex = userList.findIndex((record) => record.phone === user.phone);
 
 		if (existingUserIndex === -1) {
@@ -39,11 +41,18 @@ io.on("connection", (socket) => {
 			userList.splice(existingUserIndex, 1, { ...user, id: socket.id });
 		}
 
+		io.emit("groups", groups);
 		io.emit("user-list", userList);
 	});
 
 	socket.on("join", (name: string) => {
 		socket.join(name);
+	});
+
+	socket.on("new-group", (name: string) => {
+		groups.push(name);
+
+		io.emit("groups", groups);
 	});
 
 	socket.on("group-message", ({ name, message }) => {
@@ -52,6 +61,8 @@ io.on("connection", (socket) => {
 		if (currentUser) {
 			io.to(name).emit("new-group-chat", {
 				message,
+				group: name,
+				id: nanoid(),
 				name: currentUser.name,
 				phone: currentUser.phone,
 			});
